35c02cccd71e8bb79f44f52116b40b135c7ef203
Yuti Shih <yutishih@gmail.com>
2022-04-18 16:18:24 -0400

    about page

--- yutishih-react/src/About.js
+++ yutishih-react/src/About.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Navigation from './Components/Navigation';
+import project_3 from "./Images/test-3.png";
 
 function About() {
   return (
     <div className="About">
       <div className='container'>
         <Navigation />
+        <div className='content-wrapper'>
+          <div className='row'>
+            <div className='image-section'>
+              <img src={project_3} />
+            </div>
+            <div className="text-section">
+              <h3>About Yuti</h3>
+              <h4>Introductions</h4>
+              <p>
+                Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+              </p>
+              <a href="">Explore My Projects</a>
+              <a href="">Connect With Me <span>&#x27F6;</span></a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
